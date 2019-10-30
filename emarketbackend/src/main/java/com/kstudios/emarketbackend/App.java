f507c4dc3729077fe5381b2423f7bac1aba9b22a
Khaled Ahmed <khaled_pro93@yahoo.com>
2019-10-30 10:11:53 +0200

    Project setup successfully!

---
+++ emarketbackend/src/main/java/com/kstudios/emarketbackend/App.java
@@ -0,0 +1,13 @@
+package com.kstudios.emarketbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
